c061bb0a34dc29211183c2d77804f642dd2b93af
Otis Sutton <chop@DESKTOP-5NVK6P0.localdomain>
2022-11-29 20:04:57 +1300

    Add check for windows OS

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,30 +1,31 @@
 local wezterm = require 'wezterm'
 
+local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 local wsl_domains = wezterm.default_wsl_domains()
 
--- @TODO only load on Windows
+
 -- https://wezfurlong.org/wezterm/config/lua/wezterm/hostname.html
 for _, dom in ipairs(wsl_domains) do
     dom.default_cwd = "~"
 end
 
--- May need this for Powerline
--- font = wezterm.font_with_fallback {"Berkeley Mono", "Symbols Nerd Font Mono"},
-
 return {
-    -- @TODO only load on Windows
-    -- https://wezfurlong.org/wezterm/config/lua/wezterm/hostname.html
-    -- default_domain = 'WSL:openSUSE-Tumbleweed',
+    -- Windows
+    default_domain = is_windows and 'WSL:openSUSE-Tumbleweed' or nil,
     wsl_domains = wsl_domains,
+
     -- Tabs
+    tab_bar_at_bottom = true,
     use_fancy_tab_bar = false,
     hide_tab_bar_if_only_one_tab = true,
-    tab_bar_at_bottom = true,
+
     -- All systems
     font = wezterm.font "Berkeley Mono",
     font_size = 18.0,
+
     -- Other gruvbox theme has incorrect background color
     color_scheme = "Gruvbox dark, hard (base16)",
+
     -- Disable close prompt
     window_close_confirmation = "NeverPrompt"
 }
