313ab4652d39ce88e179438585b2811517d74f37
Otis Sutton <chop@DESKTOP-HPDPELO.localdomain>
2023-01-08 21:50:50 +1300

    New tab colors and color scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,23 +10,40 @@ for _, dom in ipairs(wsl_domains) do
 end
 
 return {
-    -- Windows
-    default_domain = is_windows and 'WSL:openSUSE-Tumbleweed' or nil,
-    wsl_domains = wsl_domains,
+  -- Colors
+  -- Based on `night_owl` theme
+  colors = {
+    tab_bar = {
+      background = '#112630',
+      active_tab = {
+        bg_color = '#011627',
+        fg_color = '#D6DEEB',
+      },
+      inactive_tab = {
+        bg_color = '#112630',
+        fg_color = '#D6DEEB',
+      },
+    },
+  },
+    
+  -- Windows
+  default_domain = is_windows and 'WSL:openSUSE-Tumbleweed' or nil,
+  wsl_domains = wsl_domains,
 
-    -- Tabs
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false,
-    hide_tab_bar_if_only_one_tab = true,
-    switch_to_last_active_tab_when_closing_tab = true,
+  -- Tabs
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  show_new_tab_button_in_tab_bar = false,
+  switch_to_last_active_tab_when_closing_tab = true,
 
-    -- All systems
-    font = wezterm.font "Berkeley Mono",
-    font_size = is_windows and 16.0 or 18.0,
+  -- All systems
+  font = wezterm.font "Berkeley Mono",
+  font_size = is_windows and 16.0 or 18.0,
 
-    -- Other gruvbox theme has incorrect background color
-    color_scheme = "Gruvbox dark, hard (base16)",
+  -- color_scheme = "Gruvbox dark, hard (base16)",
+  color_scheme = "NightOwl (Gogh)",
 
-    -- Disable close prompt
-    window_close_confirmation = "NeverPrompt"
+  -- Disable close prompt
+  window_close_confirmation = "NeverPrompt"
 }
