fb833d99726a3d4528f2a2143e2f5c49612a1cd0
Otis Sutton <otis@Clares-MacBook-Pro.local>
2023-04-14 12:59:50 +1200

    Remove resize window decorators

--- roles/config-wezterm/files/wezterm.lua
+++ roles/config-wezterm/files/wezterm.lua
@@ -26,7 +26,7 @@ return {
   -- Tabs
   tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
+  -- window_decorations = "RESIZE",
   hide_tab_bar_if_only_one_tab = true,
   show_new_tab_button_in_tab_bar = false,
   switch_to_last_active_tab_when_closing_tab = true,
