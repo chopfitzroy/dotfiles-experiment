33668ea5f4090dac4ae29c275ca9733f11d308c3
Otis Sutton <otis@myvirtualmission.com>
2023-08-29 13:11:52 +1200

    Clean up WezTerm tab bar

--- roles/config-wezterm/files/wezterm.lua
+++ roles/config-wezterm/files/wezterm.lua
@@ -20,13 +20,12 @@ return {
   },
     
   -- Windows
-  default_domain = is_windows and 'WSL:openSUSE-Tumbleweed' or nil,
   wsl_domains = wsl_domains,
+  default_domain = is_windows and 'WSL:openSUSE-Tumbleweed' or nil,
 
   -- Tabs
-  tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
-  -- window_decorations = "RESIZE",
+  tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
   show_new_tab_button_in_tab_bar = false,
   switch_to_last_active_tab_when_closing_tab = true,
@@ -34,8 +33,28 @@ return {
   -- All systems
   font = wezterm.font "Berkeley Mono",
   font_size = is_windows and 16.0 or 20.0,
-  color_scheme = "Everforest Dark (Gogh)",
 
   -- Disable close prompt
-  window_close_confirmation = "NeverPrompt"
+  window_close_confirmation = "NeverPrompt",
+
+  -- Colors
+  color_scheme = "Everforest Dark (Gogh)",
+  colors = {
+    -- https://wezfurlong.org/wezterm/config/appearance.html#tab-bar-appearance-colors
+    tab_bar = {
+      background = '#2f383e',
+      active_tab = {
+        bg_color = '#475258',
+        fg_color = '#d3c6aa',
+      },
+      inactive_tab = {
+        bg_color = '#2f383e',
+        fg_color = '#d3c6aa',
+      },
+      inactive_tab_hover = {
+        bg_color = '#475258',
+        fg_color = '#d3c6aa',
+      },
+    },
+  },
 }
